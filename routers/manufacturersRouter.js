a53a8f7406fb4e2a4beb35ca29c1f4b2cbd5eef2
Kyrylo <fresh.js.ftd.@gmail.com>
2025-01-31 20:25:01 +0200

    Add route method usage example

--- routers/manufacturersRouter.js
+++ routers/manufacturersRouter.js
@@ -1,11 +1,29 @@
 const manufacturerRouter = require('express').Router();
 const ManufacturersController = require('../controllers/manufacturersController');
 
-manufacturerRouter.post('/', ManufacturersController.createManufacturer);
-manufacturerRouter.get('/', ManufacturersController.getManufacturers);
+// manufacturerRouter.post('/', ManufacturersController.createManufacturer);
+// manufacturerRouter.get('/', ManufacturersController.getManufacturers);
+manufacturerRouter
+  .route('/')
+  .post(ManufacturersController.createManufacturer)
+  .get(ManufacturersController.getManufacturers);
 
-manufacturerRouter.get('/:manufacturerId', ManufacturersController.getManufacturer);
-manufacturerRouter.put('/:manufacturerId', ManufacturersController.updateManufacturer);
-manufacturerRouter.delete('/:manufacturerId', ManufacturersController.deleteManufacturer);
+// manufacturerRouter.get(
+//   '/:manufacturerId',
+//   ManufacturersController.getManufacturer
+// );
+// manufacturerRouter.put(
+//   '/:manufacturerId',
+//   ManufacturersController.updateManufacturer
+// );
+// manufacturerRouter.delete(
+//   '/:manufacturerId',
+//   ManufacturersController.deleteManufacturer
+// );
+manufacturerRouter
+  .route('/:manufacturerId')
+  .get(ManufacturersController.getManufacturer)
+  .put(ManufacturersController.updateManufacturer)
+  .delete(ManufacturersController.deleteManufacturer);
 
 module.exports = manufacturerRouter;
