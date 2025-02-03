4a97625fce5c5eabd041384b6beb020ffa922d65
Kyrylo <fresh.js.ftd.@gmail.com>
2025-02-03 19:37:36 +0200

    Add model reexport example

--- models/index.js
+++ models/index.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
 const dbConfig = require('../config/db.json');
+const Manufacturer = require('./manufacturer');
 
 async function main(params) {
   await mongoose.connect(dbConfig.connectionString);
 }
 
 main().catch((err) => console.log(err));
+
+module.exports = {
+  Manufacturer
+};
